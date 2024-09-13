e434dcefea53d40e744c918e4df0f7258b5ff891
Farhad Gulizada <62217940+ferhadquluzade@users.noreply.github.com>
2024-09-14 01:10:48 +0200

    Add files via upload

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,6 @@ import { getCpuInfo } from "./subcmd/cpu.js";
 import { getOsInfo } from "./subcmd/os.js";
 import { getRamInfo } from "./subcmd/ram.js";
 import { getEnvInfo } from "./subcmd/env.js";
-import { getUserInfo } from "./subcmd/user.js";
-import { getHostInfo } from "./subcmd/host.js";
-import { getGpuInfo } from "./subcmd/gpu.js";
 
 async function main() {
   try {
@@ -17,45 +14,30 @@ async function main() {
       "--os": Boolean,
       "--ram": Boolean,
       "--env": Boolean,
-      "--user": Boolean,
-      "--host": Boolean,
-      "--gpu": Boolean,
     });
 
     const option = Object.keys(args).find((key) => args[key] === true);
 
     switch (option) {
-      case "--gpu":
-        const gpuInfo = await getGpuInfo();
-        console.log(gpuInfo);
-        break;
-      case "--host":
-        const hostInfo = getHostInfo();
-        console.log(hostInfo);
-        break;
-      case "--user":
-        const userInfo = getUserInfo();
-        console.log(userInfo);
-        break;
       case "--env":
         const envInfo = getEnvInfo();
-        console.log(envInfo);
+        console.log("Environment variables:", envInfo);
         break;
       case "--ram":
-        const ramInfo = await getRamInfo();
-        console.log(ramInfo);
+        const ramInfo = getRamInfo();
+        console.log("RAM/Memory:", ramInfo);
         break;
       case "--cpu":
         const cpuInfo = getCpuInfo();
-        console.log(cpuInfo);
+        console.log("CPU:", cpuInfo);
         break;
       case "--ip":
         const ipInfo = await getIpInfo();
-        console.log(ipInfo);
+        console.log("Ip:", ipInfo);
         break;
       case "--os":
         const osInfo = getOsInfo();
-        console.log(osInfo);
+        console.log("OS:", osInfo);
         break;
       default:
         usage();
@@ -69,14 +51,11 @@ async function main() {
 
 function usage() {
   console.log(`Usage: get --[FLAG]
---ip\tIp address (public v4 and local)
+--ip\tIp address (v4 and local)
 --cpu\tCPU information
 --os\tOS information
 --ram\tRAM/Memory usage
---env\tEnvironment variables
---user\tUser information
---host\tHost information
---gpu\tGPU information`);
+--env\tEnvironment variables`);
 }
 
 main();
